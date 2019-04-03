d7b6c8d4129913f3a64e4e87a1a15dcc7edaee38
Juan Carlos Chica Ureña <juancarlos.boxing@gmail.com>
2019-04-03 12:19:34 +0200

    falta rss

---
+++ assets/js/index.js
@@ -0,0 +1,9 @@
+/**
+ * La función $(document).ready() de jQuery 
+ * ejecuta este código cuando se carga totalmente
+ * la página (index.html).
+ */
+
+$(document).ready(function () {
+    $.controlador.init("panel_inicio");
+});
